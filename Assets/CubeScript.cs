db585044a2dd057290af86a765d5576a38c2919b
Andrea Amatucci <amatucci.andrea@gmail.com>
2020-05-02 00:10:40 +0200

    code cleanup

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -9,14 +9,8 @@ public class CubeScript : MonoBehaviour
 
     private float maxOffset = 5.5f;
 
-    // Update is called once per frame
     void Update()
     {
-        // float noise = Mathf.PerlinNoise(transform.position.x, transform.position.y);
-        // var variation = Mathf.Sin(Time.time) + 1f;
-        // Vector3 temp = new Vector3(1f, variation * offset, 1f);
-        // transform.position = temp;
-
         float perlin = Mathf.PerlinNoise(transform.position.x / CubeScript.div + Time.time, transform.position.z / CubeScript.div + Time.time);
         transform.localScale = new Vector3(1f, perlin * maxOffset, 1f);
     }
